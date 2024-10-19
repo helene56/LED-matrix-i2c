79684e68eb513dbc74de48ac9f945549d2731838
helene <helene.maja@hotmail.dk>
2024-10-19 16:35:43 +0200

    rewrite joystick.cpp to read from the joystick register collected in one function

--- joystick.cpp
+++ joystick.cpp
@@ -1,33 +1,58 @@
 #include "joystick.h"
 #include <stdio.h>
 
-void read_joystick(std::uint8_t *buffer_dst)
+std::uint8_t read_joystick()
 {
 
-    std::uint8_t register_address = 0x2B; // Address of PORTD
-
-    // Step 1: Send the register address (PORTD) to the ATTINY88
-    int result = i2c_write_blocking(I2C_PORT, ATtiny_address, &register_address, 1, true); // true for 'repeated start'
+    std::uint8_t joystick_reg = JOYSTICK_REG; // Address of PORTD
+    std::uint8_t joystick_state {0};
+    // write to the joystick register
+    int result = i2c_write_blocking(I2C_PORT, ATtiny_address, &joystick_reg, 1, true); // true for 'repeated start'
     if (result < 0)
     {
         printf("I2C write failed\n");
-        return;
     }
-    // printf("I2C write result: %d\n", result);
-
-    sleep_us(10);
-
-    result = i2c_read_blocking(I2C_PORT, ATtiny_address, buffer_dst, 
+    // read the byte at the previous write location, then store the result in joystick_state
+    result = i2c_read_blocking(I2C_PORT, ATtiny_address, &joystick_state, 
                                    1, false);
 
     if (result < 0)
     {
         printf("I2C read failed\n");
     }
-    // printf("I2C read result: %d\n", result);
 
-    // else
-    // {
-    //     printf("Data read succesfully\n");
-    // }
+    return joystick_state;
+}
+
+void check_joystick(std::uint8_t joystick_state)
+{
+    // note the directions will defer if the HAT is turned, 
+    // so print statement is set to subjective values
+    if (joystick_state & 0x01)
+    {
+        // 0000'0001
+        printf("joystick down\n");
+    }
+    if (joystick_state & 0x02)
+    {
+        // 0000'0010
+        printf("joystick right\n");
+    }
+    if (joystick_state & 0x04)
+    {
+        //0000'0100
+        printf("joystick up\n");
+    }
+    if (joystick_state & 0x08)
+    {
+        //0000'1000
+        printf("joystick pressed\n");
+    }
+    if (joystick_state & 0x10)
+    {
+        // 0001'0000
+        printf("joystick left\n");
+    }
+
+
 }
